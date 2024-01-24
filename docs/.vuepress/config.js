328e4e4f6d450d5c5292b0f6102d4a304d8a62db
qilang <742093571@qq.com>
2024-01-24 21:48:23 +0800

    测试
    Signed-off-by: qilang <742093571@qq.com>

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,38 @@
+module.exports = {
+    title: '红歌',
+    description: '红歌的博客网站',
+    base: '/lang.github.io/',
+    locales: {
+        '/': {
+          lang: 'zh-CN'
+        }
+    },
+    themeConfig: {
+      nav: [
+        { text: '首页', link: '/' },
+        {
+          text: '红歌的博客',
+          items: [
+            { text: 'CSDN', link: 'https://www.csdn.net/' },
+            {
+              text: '掘金',
+              link: 'https://juejin.cn/',
+            },
+          ],
+        },
+      ],
+      sidebar: [
+        {
+          title: '网站介绍',
+          path: '/',
+        },
+        {
+          title: '学习文档',
+          children: [
+            { title: 'javascript', path: '/document/Javascript' },
+            { title: 'node', path: '/document/Node' },
+          ],
+        },
+      ],
+    },
+  };
